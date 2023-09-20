d63ec080019afa8cc681b0b26f3dfd1fbb980b56
potla393 <79921095+potla393@users.noreply.github.com>
2023-09-20 18:13:48 +0530

    Create buid.groovy

---
+++ vars/buid.groovy
@@ -0,0 +1,17 @@
+def call(String mvngoal ) {
+
+  if ("${mvngoal}" == "Clean) {
+        sh "mvn clean"
+      }
+   else if ("${mvngoal}" == "Compile) {
+        sh "mvn clean compile"
+      }
+   else if ("${mvngoal}" == "Test) {
+        sh "mvn clean compile test"
+      }
+
+  else if ("${mvngoal}" == "Package) {
+        sh "mvn clean compile test package"
+      }
+
+}
